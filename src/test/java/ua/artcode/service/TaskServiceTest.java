d005e9e7006275e6b8321ee6a0d58cb09823c753
jed1knight <obonemax@gmail.com>
2015-11-06 17:35:18 +0200

    TaskServiseTest was changed

--- src/test/java/ua/artcode/service/TaskServiceTest.java
+++ src/test/java/ua/artcode/service/TaskServiceTest.java
@@ -2,6 +2,7 @@ package ua.artcode.service;
 
 import org.apache.log4j.Logger;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,10 +14,13 @@ import ua.artcode.exception.AppException;
 import ua.artcode.exception.NoSuchTaskException;
 import ua.artcode.model.CodingBatTask;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.TreeMap;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 //TODO finish tests
@@ -26,6 +30,10 @@ public class TaskServiceTest {
     private static final Logger LOGGER = Logger.getLogger(TaskServiceTest.class);
     private static SimpleTaskDao mockTaskDao;
     private CodingBatTaskContainer taskContainerMock;
+
+    private CodingBatTaskContainer taskContainer;
+    private SimpleTaskDaoImpl simpleTaskDao;
+    private SimpleTaskServiceImpl simpleTaskService;
 
     @BeforeClass
     public static void initMocks(){
@@ -38,62 +46,53 @@ public class TaskServiceTest {
         }
     }
 
+    @Before
+    public void initTasks() {
+        taskContainer = new CodingBatTaskContainer();
+        simpleTaskDao = new SimpleTaskDaoImpl(taskContainer);
+        simpleTaskService = new SimpleTaskServiceImpl(simpleTaskDao);
+
+        CodingBatTask mockTask1 = mock(CodingBatTask.class);
+        CodingBatTask mockTask2 = mock(CodingBatTask.class);
+        CodingBatTask mockTask3 = mock(CodingBatTask.class);
+        CodingBatTask mockTask4 = mock(CodingBatTask.class);
+        CodingBatTask mockTask5 = mock(CodingBatTask.class);
+
+        when(mockTask1.getId()).thenReturn("0");
+        when(mockTask2.getId()).thenReturn("1");
+        when(mockTask3.getId()).thenReturn("2");
+        when(mockTask4.getId()).thenReturn("3");
+        when(mockTask5.getId()).thenReturn("4");
+
+        taskContainer.addTask(mockTask1);
+        taskContainer.addTask(mockTask2);
+        taskContainer.addTask(mockTask3);
+        taskContainer.addTask(mockTask4);
+        taskContainer.addTask(mockTask5);
+
+    }
+
+//    @After
+//    public void cleanContainer() {
+//        simpleTaskService = null;
+//        simpleTaskDao = null;
+//        taskContainer = null;
+//    }
+
 
     @Test(expected = NoSuchTaskException.class)
     public void findTaskByIdWithNoTask() throws NoSuchTaskException {
-        taskContainerMock = mock(CodingBatTaskContainer.class);
-
-        SimpleTaskDaoImpl dao = new SimpleTaskDaoImpl(taskContainerMock);
-
-        dao.findById("notId");
+        simpleTaskService.getTask("noId");
     }
 
     @Test
     public void getAllTasks() throws AppException {
-        CodingBatTaskContainer taskContainer = new CodingBatTaskContainer();
-        SimpleTaskDaoImpl dao = new SimpleTaskDaoImpl(taskContainer);
-        SimpleTaskServiceImpl simpleTaskService = new SimpleTaskServiceImpl(dao);
-        CodingBatTask mockTask1 = mock(CodingBatTask.class);
-        CodingBatTask mockTask2 = mock(CodingBatTask.class);
-        CodingBatTask mockTask3 = mock(CodingBatTask.class);
-        CodingBatTask mockTask4 = mock(CodingBatTask.class);
-        CodingBatTask mockTask5 = mock(CodingBatTask.class);
-
-        taskContainer.addTask(mockTask1);
-        taskContainer.addTask(mockTask2);
-        taskContainer.addTask(mockTask3);
-        taskContainer.addTask(mockTask4);
-        taskContainer.addTask(mockTask5);
-
         assertEquals(5, simpleTaskService.getAll().size());
     }
 
-
     @Test
     public void getTask() throws NoSuchTaskException {
-        CodingBatTaskContainer taskContainer = new CodingBatTaskContainer();
-        SimpleTaskDaoImpl dao = new SimpleTaskDaoImpl(taskContainer);
-        SimpleTaskServiceImpl simpleTaskService = new SimpleTaskServiceImpl(dao);
-        CodingBatTask mockTask1 = mock(CodingBatTask.class);
-        CodingBatTask mockTask2 = mock(CodingBatTask.class);
-        CodingBatTask mockTask3 = mock(CodingBatTask.class);
-        CodingBatTask mockTask4 = mock(CodingBatTask.class);
-        CodingBatTask mockTask5 = mock(CodingBatTask.class);
-
-        when(mockTask1.getId()).thenReturn("1875");
-        when(mockTask2.getId()).thenReturn("1876");
-        when(mockTask3.getId()).thenReturn("1877");
-        when(mockTask4.getId()).thenReturn("1878");
-        when(mockTask5.getId()).thenReturn("1879");
-
-        taskContainer.addTask(mockTask1);
-        taskContainer.addTask(mockTask2);
-        taskContainer.addTask(mockTask3);
-        taskContainer.addTask(mockTask4);
-        taskContainer.addTask(mockTask5);
-//        System.out.println(simpleTaskService.getTask("2"));
-
-        assertEquals(simpleTaskService.getTask("7").getId(), "1877");
+        assertEquals("2", simpleTaskService.getTask("12").getId());
     }
 
 }
