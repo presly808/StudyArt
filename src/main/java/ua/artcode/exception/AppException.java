39e28c682eea86a225595c729381287fb93702c0
presly808 <presly808@gmail.com>
2015-11-06 13:58:39 +0200

    added validator

--- src/main/java/ua/artcode/exception/AppException.java
+++ src/main/java/ua/artcode/exception/AppException.java
@@ -1,11 +1,30 @@
 package ua.artcode.exception;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 /**
  * Created by serhii on 28.10.15.
  */
 public class AppException extends Exception {
 
+    private Collection<String> exceptionMessageList;
+
+    public AppException() {
+    }
+
     public AppException(String message) {
         super(message);
+        exceptionMessageList = new LinkedList<>();
+        exceptionMessageList.add(message);
     }
+
+    public void addMessage(String message){
+        exceptionMessageList.add(message);
+    }
+
+    public Collection<String> getExceptionMessageList(){
+        return exceptionMessageList;
+    }
+
 }
--- src/main/java/ua/artcode/service/SimpleUserServiceImpl.java
+++ src/main/java/ua/artcode/service/SimpleUserServiceImpl.java
@@ -5,15 +5,19 @@ import ua.artcode.exception.AppException;
 import ua.artcode.exception.UserAuthenticationFailException;
 import ua.artcode.model.AccountType;
 import ua.artcode.model.UserAccount;
+import ua.artcode.validation.Validator;
 
 import java.util.Set;
 
 public class SimpleUserServiceImpl implements SimpleUserService {
 
+
+    private Validator<UserAccount> accountValidator;
     private SimpleUserDao userDao;
 
-    public SimpleUserServiceImpl(SimpleUserDao userDao) {
+    public SimpleUserServiceImpl(SimpleUserDao userDao, Validator<UserAccount> accountValidator) {
         this.userDao = userDao;
+        this.accountValidator = accountValidator;
     }
 
     @Override
@@ -29,6 +33,9 @@ public class SimpleUserServiceImpl implements SimpleUserService {
 
     @Override
     public UserAccount register(String username, String password, String email, AccountType accountType) throws AppException {
+
+        accountValidator.validate(new UserAccount(username,password,email));
+
         return userDao.create(new UserAccount(username, password, email, accountType));
     }
 
