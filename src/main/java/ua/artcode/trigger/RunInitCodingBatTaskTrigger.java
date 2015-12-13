54666721bfc17cedb3fe57c0484c527068066236
Chernyshovdev <Chernyshov.dev@gmail.com>
2015-12-13 21:50:58 +0200

    change main

--- src/main/java/ua/artcode/trigger/RunInitCodingBatTaskTrigger.java
+++ src/main/java/ua/artcode/trigger/RunInitCodingBatTaskTrigger.java
@@ -1,21 +1,18 @@
 package ua.artcode.trigger;
 
-import org.springframework.context.ApplicationContext;
-import ua.artcode.dao.SimpleTaskDao;
-import ua.artcode.dao.SimpleTaskDaoMongoImpl;
-import ua.artcode.db.DataBaseManager;
-import ua.artcode.utils.SpringContext;
-
 public class RunInitCodingBatTaskTrigger {
 
     public static void main(String[] args) {
-        InitCodingBatTaskTrigger.loadTasksIfNeeded();
+        //InitCodingBatTaskTrigger.loadTasksIfNeeded();
         //InitCodingBatTaskTrigger.loadTasksToDataBase();
-       /* ApplicationContext context = SpringContext.getContext();
-        DataBaseManager dataBaseManager = (DataBaseManager) context.getBean("dbManager");
-        SimpleTaskDao simpleTaskDao = new SimpleTaskDaoMongoImpl(dataBaseManager);
-        dataBaseManager.restoreDataBaseFromDump();*/
-        //dataBaseManager.createDumpOfDataBase();
-        //System.out.println(simpleTaskDao.size());
+//        ApplicationContext context = SpringContext.getContext();
+//        Morphia morphia = context.getBean(Morphia.class);
+//        morphia.map(CodingBatTask.class);
+//        Datastore datastore = context.getBean(Datastore.class);
+//        SimpleTaskDao simpleTaskDao = new SimpleTaskDaoMongoImpl(datastore);
+        //InitCodingBatTaskTrigger.loadTasksIfNeeded();
+        InitCodingBatTaskTrigger.loadTasksToDataBase();
+        //InitCodingBatTaskTrigger.createDumpOfDataBase();
+        //InitCodingBatTaskTrigger.restoreDataBaseFromDump();
     }
 }
