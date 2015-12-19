c47486ea1ec2a4d9df6cae0da81d85c57ad120f3
Chernyshovdev <Chernyshov.dev@gmail.com>
2015-12-19 18:01:27 +0200

    change user dao

--- src/main/java/ua/artcode/dao/UserDao.java
+++ src/main/java/ua/artcode/dao/UserDao.java
@@ -5,12 +5,11 @@ import ua.artcode.exception.UserAccountExistException;
 import ua.artcode.model.common.User;
 
 import java.util.List;
-import java.util.Set;
 
 
 public interface UserDao {
 
-    User create(User user) throws UserAccountExistException;
+    User addUser(User user) throws UserAccountExistException;
 
     User findByUserName(String username) throws NoSuchUserException;
 
