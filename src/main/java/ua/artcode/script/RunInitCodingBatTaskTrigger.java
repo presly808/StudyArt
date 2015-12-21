b246427d854ced6830e5d960404f39d0ffba2938
Chernyshovdev <Chernyshov.dev@gmail.com>
2015-12-21 16:53:06 +0200

    add script test

--- src/main/java/ua/artcode/script/RunInitCodingBatTaskTrigger.java
+++ src/main/java/ua/artcode/script/RunInitCodingBatTaskTrigger.java
@@ -1,27 +1,31 @@
 package ua.artcode.script;
 
-import org.mongodb.morphia.Datastore;
-import org.springframework.context.ApplicationContext;
 import ua.artcode.exception.UserAccountExistException;
-import ua.artcode.utils.SpringContext;
 
 public class RunInitCodingBatTaskTrigger {
 
     public static void main(String[] args) throws UserAccountExistException {
         //InitCodingBatTaskTrigger.loadTasksIfNeeded();
         //InitCodingBatTaskTrigger.loadTasksToDataBase();
-        ApplicationContext context = SpringContext.getContext();
+//        ApplicationContext context = SpringContext.getContext();
 //        Morphia morphia = context.getBean(Morphia.class);
 //        morphia.map(CodingBatTask.class);
-        Datastore datastore = (Datastore) context.getBean("datastore");
-//        SimpleTaskDao simpleTaskDao = new SimpleTaskDaoMongoImpl(datastore);
-        //InitCodingBatTaskTrigger.loadTasksIfNeeded();
-        InitCodingBatTaskTrigger.loadTasksToDataBase();
-        //InitCodingBatTaskTrigger.createDumpOfDataBase();
+//        Datastore datastore = (Datastore) context.getBean("datastore");
+//        CodingBatTaskDao codingBatTaskDao = new CodingBatTaskDaoMongoImpl(datastore);
+//        InitCodingBatTaskTrigger.loadTasksIfNeeded();
+//        InitCodingBatTaskTrigger.loadTasksToDataBase();
+         // InitCodingBatTaskTrigger.createDumpOfDataBase();
         //InitCodingBatTaskTrigger.restoreDataBaseFromDump();
 //        UserDao userDao = new UserDaoMongoImpl(datastore);
 //        userDao.addUser(new User("Razer","111","chernyshov.dev@gmail.com", UserType.ADMIN));
 //        userDao.addUser(new User("Maxim","222","obonemax@gmail.com", UserType.ADMIN));
-        InitCodingBatTaskTrigger.createDumpOfDataBase();
+//        InitCodingBatTaskTrigger.createDumpOfDataBase();
+//        CodingBatTask codingBatTask = new CodingBatTask();
+//        codingBatTask.setCodingBatId("00000");
+//        codingBatTaskDao.addTask(codingBatTask);
+//        CodingBatTask c2 = new CodingBatTask();
+//        c2.setCodingBatId("5555");
+//        System.out.println(codingBatTaskDao.isExist(codingBatTask));
+
     }
 }
---
+++ src/test/java/ua/artcode/script/InitCodingBatTaskTriggerTest.java
@@ -0,0 +1,66 @@
+package ua.artcode.script;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ua.artcode.utils.io.AppPropertiesHolder;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Razer on 19.12.15.
+ */
+public class InitCodingBatTaskTriggerTest {
+    private static String nameOfDb;
+    private static File dump;
+    private static File dataBase;
+    private static File original;
+
+    @BeforeClass
+    public static void copyOriginalDump() {
+        nameOfDb = AppPropertiesHolder.getProperty("mongo.db");
+        dump = new File("dump");
+        if (!dump.exists()) {
+            dump.mkdir();
+            dump.deleteOnExit();
+        }
+        {
+            dataBase = new File(dump + "/" + nameOfDb);
+            if (dataBase.exists()) {
+                original = new File(dump + "/" + "original");
+                dataBase.renameTo(original);
+            }
+        }
+    }
+
+    public static void removeDirectory(File dir) {
+        if (dir.isDirectory()) {
+            File[] files = dir.listFiles();
+            if (files != null && files.length > 0) {
+                for (File aFile : files) {
+                    removeDirectory(aFile);
+                }
+            }
+            dir.delete();
+        } else {
+            dir.delete();
+        }
+    }
+
+    @Test
+    public void createDumpOfDataBaseTest() {
+        InitCodingBatTaskTrigger.createDumpOfDataBase();
+
+        assertEquals(true, dataBase.exists());
+    }
+
+    @AfterClass
+    public static void deleteDump() {
+        removeDirectory(dataBase);
+        if (original != null) {
+            original.renameTo(dataBase);
+        }
+    }
+}
