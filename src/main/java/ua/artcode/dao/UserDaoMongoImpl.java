63ec3dfc43620e573a2ce46845a0de3c145c9053
jed1knight <obonemax@gmail.com>
2016-01-05 19:42:46 +0200

    added update method, exist modified

--- src/main/java/ua/artcode/dao/UserDaoMongoImpl.java
+++ src/main/java/ua/artcode/dao/UserDaoMongoImpl.java
@@ -3,6 +3,7 @@ package ua.artcode.dao;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.log4j.Logger;
 import org.mongodb.morphia.Datastore;
+import ua.artcode.exception.AppException;
 import ua.artcode.exception.AppValidationException;
 import ua.artcode.exception.NoSuchUserException;
 import ua.artcode.exception.UserAccountExistException;
@@ -23,11 +24,11 @@ public class UserDaoMongoImpl implements UserDao {
     }
 
     @Override
-    public User addUser(User user) throws AppValidationException, UserAccountExistException {
+    public User addUser(User user) throws AppException {
         UserValidator validator = new UserValidator();
 
         validator.validate(user);
-        if (isExist(user)) {
+        if (isExist(user.getEmail())) {
             throw new UserAccountExistException("User with this account already exist.");
         }
         // Encryption password to MD5
@@ -60,14 +61,26 @@ public class UserDaoMongoImpl implements UserDao {
     }
 
     @Override
+    public User update(String email, User newUser) throws AppException {
+        User oldUser = findByUserEmail(email);
+        newUser.setEmail(oldUser.getEmail());
+        delete(email);
+        addUser(newUser);
+        return newUser;
+    }
+
+    @Override
     public List<User> getAllUser() {
         return datastore.find(User.class).asList();
     }
 
     @Override
-    public boolean isExist(User user) {
-
-        return false;
+    public boolean isExist(String userEmail) {
+        User existUser = datastore.find(User.class).field("email").equal(userEmail).get();
+        if (existUser == null) {
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -75,7 +88,5 @@ public class UserDaoMongoImpl implements UserDao {
     public int size() {
         return (int) datastore.getDB().getCollection("User").count();
     }
-
-
 
 }
