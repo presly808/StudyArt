e3267ff5e0103bb171b5ae7803dfd730b40669b8
Chernyshovdev <Chernyshov.dev@gmail.com>
2016-01-14 16:43:19 +0200

    create FindTask using jsp

--- src/main/java/ua/artcode/servlet/FindTask.java
+++ src/main/java/ua/artcode/servlet/FindTask.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Created by Razer on 10.01.16.
@@ -16,21 +15,6 @@ public class FindTask extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter pw = resp.getWriter();
-        pw.println("<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head>          \n" +
-                "<meta charset=\"UTF-8\">\n" +
-                "<title>StudyArt</title>\n" +
-                "</head> \n" +
-                "<body>\n" +
-                "<form  >\n" +
-                "<p><input name=\"taskId\"></p>\n" +
-                "<p><input value=\"get task\"  type=\"submit\"></p>\n" +
-                "</form>\n" +
-                "</body>\n" +
-                "</html>");
-        pw.flush();
-        resp.sendRedirect("http://www.google.com");
+        req.getRequestDispatcher("/pages/find-task.jsp").forward(req, resp);
     }
 }
