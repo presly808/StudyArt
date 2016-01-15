1b2c9afe1fe67ea3e2a08da9a02d488d852bc6a2
Chernyshovdev <Chernyshov.dev@gmail.com>
2016-01-15 21:56:11 +0200

    add CheckTask servlet

--- src/main/java/ua/artcode/servlet/CheckTask.java
+++ src/main/java/ua/artcode/servlet/CheckTask.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Maxim on 14.01.2016.
@@ -14,6 +15,24 @@ import java.io.IOException;
 public class CheckTask extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+//        String userCode = req.getParameter("userCode");
+          String id=req.getParameter("id");
+//        ApplicationContext context = SpringContext.getContext();
+//        Datastore datastore = (Datastore) context.getBean("datastore");
+//        CodingBatTaskDao codingBatTaskDao = new CodingBatTaskDaoMongoImpl(datastore);
+//        CodingBatTask task = null;
+//        try {
+//            task = codingBatTaskDao.findById(id);
+//        } catch (NoSuchTaskException e) {
+//            e.printStackTrace();
+//        }
+        PrintWriter pw = resp.getWriter();
+        pw.println("<body>" +
+                "<h1>Hello Servlet</h1>" +
+                "<p>info from servlet " +id +"</p>" +
+                "</body>");
+        pw.flush();
+        //TaskRunFacade taskRunFacade = context.getBean(TaskRunFacade.class);
+        //taskRunFacade.runTask(task, task.getTemplate().substring(0, task.getTemplate().length() - 1) + userCode+"\n}");
     }
 }
