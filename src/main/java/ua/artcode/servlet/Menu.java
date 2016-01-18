1e9e263158f23f42c3ace78c021f76dc995952fe
Chernyshovdev <Chernyshov.dev@gmail.com>
2016-01-18 17:30:35 +0200

    create Menu servlet

---
+++ src/main/java/ua/artcode/servlet/Menu.java
@@ -0,0 +1,19 @@
+package ua.artcode.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Razer on 18.01.16.
+ */
+@WebServlet(urlPatterns = "/menu")
+public class Menu extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/pages/menu.jsp").forward(req, resp);
+    }
+}
