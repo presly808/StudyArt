02e0511cba570b83fa967a050699147282177297
Chernyshovdev <Chernyshov.dev@gmail.com>
2016-01-19 21:52:08 +0200

    add annotation

--- src/main/java/ua/artcode/servlet/Registration.java
+++ src/main/java/ua/artcode/servlet/Registration.java
@@ -1,6 +1,7 @@
 package ua.artcode.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +10,7 @@ import java.io.IOException;
 /**
  * Created by Razer on 19.01.16.
  */
+@WebServlet(urlPatterns = "/registration")
 public class Registration extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
