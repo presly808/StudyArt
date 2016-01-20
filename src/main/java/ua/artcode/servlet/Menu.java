9e309bd22c06a8b9a0787a9ad7f993ed853acf22
Chernyshovdev <Chernyshov.dev@gmail.com>
2016-01-20 15:49:40 +0200

    change method to post

--- src/main/java/ua/artcode/servlet/Menu.java
+++ src/main/java/ua/artcode/servlet/Menu.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/menu")
 public class Menu extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/pages/menu.jsp").forward(req, resp);
     }
 }
