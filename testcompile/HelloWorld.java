b64f6b223547dbf5a594dfc4da8285222835c6d1
presly808 <presly808@gmail.com>
2016-06-21 06:29:19 +0300

    add test comiple

---
+++ testcompile/HelloWorld.java
@@ -0,0 +1,6 @@
+package testcompile;
+public class HelloWorld implements ua.artcode.experimental.compiling.InlineCompiler.DoStuff {
+    public void doStuff() {
+        System.out.println("Hello world");
+    }
+}
